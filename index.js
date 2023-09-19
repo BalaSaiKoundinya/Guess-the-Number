817743698fa878a40331170bb681c4041c67e4f1
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 20:20:25 +0530

    Updated code for too high number than random number

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let gameResult = document.getElementById("gameResult");
 let inputResult = document.getElementById("inputResult");
 
+let randomNumber = Math.ceil(Math.random() * 100);
+console.log(randomNumber);
 function checkGuess() {
-    
+    let guessedNumber = parseInt(userInput.value);
+
+    if (guessedNumber > randomNumber) {
+        gameResult.textContent = "Too High, Try Again!";
+        gameResult.style.backgroundColor = "#1e217c";
+    }
 }
