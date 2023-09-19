7a10514851f9a8dfd132bad0d3e50d353ef84727
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 20:36:38 +0530

    removed console.log from code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let gameResult = document.getElementById("gameResult");
 let inputResult = document.getElementById("inputResult");
 
 let randomNumber = Math.ceil(Math.random() * 100);
-console.log(randomNumber);
+
 function checkGuess() {
     let guessedNumber = parseInt(userInput.value);
 
