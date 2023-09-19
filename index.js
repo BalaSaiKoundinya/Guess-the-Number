2b5fcd2a91ab62b43a27074553c8a4fb5499c2a5
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 20:28:12 +0530

    Code for both guessed number and random number are same.

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ function checkGuess() {
         gameResult.textContent = "Too High, Try Again!";
         gameResult.style.backgroundColor = "#1e217c";
     }
-
     else if (guessedNumber < randomNumber) {
         gameResult.textContent = "Too Low, Try Again!";
         gameResult.style.backgroundColor = "#1e217c";
+    }
+    else if (guessedNumber === randomNumber) {
+        gameResult.textContent = "Congratulations! You got it right.";
+        gameResult.style.backgroundColor = "Green";
     }
 }
